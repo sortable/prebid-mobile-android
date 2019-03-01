691e933bd313705ef563ca8ac007e133ac8dafa6
Mark Sherry <marks@sortable.com>
2019-03-01 11:24:08 -0500

    Changes to the demo app to use Sortable IDs

--- PrebidMobile/API1.0Demo/src/main/java/org/prebid/mobile/app/Constants.java
+++ PrebidMobile/API1.0Demo/src/main/java/org/prebid/mobile/app/Constants.java
@@ -27,12 +27,12 @@ public class Constants {
     static final String AUTO_REFRESH_NAME = "autoRefresh";
 
     // Prebid server config ids
-    static final String PBS_ACCOUNT_ID = "bfa84af2-bd16-4d35-96ad-31c6bb888df0";
+    static final String PBS_ACCOUNT_ID = "prebid.org";
     static final String PBS_CONFIG_ID_300x250_APPNEXUS_DEMAND = "6ace8c7d-88c0-4623-8117-75bc3f0a2e45";
     static final String PBS_CONFIG_ID_320x50_APPNEXUS_DEMAND = "625c6125-f19e-4d5b-95c5-55501526b2a4";
     static final String PBS_CONFIG_ID_INTERSTITIAL_APPNEXUS_DEMAND = "625c6125-f19e-4d5b-95c5-55501526b2a4";
     // MoPub ad unit ids
-    static final String MOPUB_BANNER_ADUNIT_ID_300x250 = "a935eac11acd416f92640411234fbba6";
+    static final String MOPUB_BANNER_ADUNIT_ID_300x250 = "a477c050091b43f4ae5bcdb410dce4f8";
     static final String MOPUB_BANNER_ADUNIT_ID_320x50 = "9dbccb87ab4d4a178450c0bc986b4571";
     static final String MOPUB_INTERSTITIAL_ADUNIT_ID = "2829868d308643edbec0795977f17437";
     // DFP ad unit ids
